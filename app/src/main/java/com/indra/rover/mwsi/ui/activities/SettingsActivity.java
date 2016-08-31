9d126325e958f8f65891313d84ae0b9c092e073c
Indra <Indra>
2016-08-31 17:45:59 +0800

    updated the color based on manilad colors added icons for new sync activity

--- app/src/main/java/com/indra/rover/mwsi/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/indra/rover/mwsi/ui/activities/SettingsActivity.java
@@ -1,14 +1,18 @@
 package com.indra.rover.mwsi.ui.activities;
 
-import android.content.Intent;
+
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.indra.rover.mwsi.R;
-import com.indra.rover.mwsi.adapters.MReaderListAdapter;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -42,11 +46,49 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         int id =  view.getId();
-        switch(id){
-            case R.id.btnMeterCodeMain:
-                Intent intent = new Intent(this, MReadersListActivity.class);
-                startActivity(intent);
+        switch (id){
+            case R.id.btnLogin:
+                showLogin();
                 break;
         }
     }
+
+    Dialog dialog;
+    public void showLogin(){
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_login);
+        dialog.setCancelable(false);
+        final EditText txtDlg = (EditText)dialog.findViewById(R.id.dlg_body);
+        ImageButton dlgBtnClose = (ImageButton)dialog.findViewById(R.id.dlg_btn_close);
+        dlgBtnClose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        Button btn =  (Button)dialog.findViewById(R.id.dlg_btn_yes);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String value =   txtDlg.getText().toString();
+                findViewById(R.id.pnl_admin_settings).setVisibility(View.VISIBLE);
+                findViewById(R.id.pnl_admin_login).setVisibility(View.GONE);
+                dialog.dismiss();
+            }
+        });
+
+        btn =  (Button)dialog.findViewById(R.id.dlg_btn_no);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
+    }
+
 }
